770c466bca535223ced5aff97f42defafee24c65
Jovanka Gulicoska <jovanka.gulicoska@keitaro.com>
2016-12-02 17:22:25 +0100

    get saas auth from env

--- ckanext/saasportal_quote/plugin.py
+++ ckanext/saasportal_quote/plugin.py
@@ -2,6 +2,7 @@ import logging
 
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.common import config
 
 from helpers import is_admin, is_member, is_editor
 
@@ -22,3 +23,8 @@ class Saasportal_QuotePlugin(plugins.SingletonPlugin):
         return {'saasportal_is_admin': is_admin,
         		'saasportal_is_member': is_member,
         		'saasportal_is_editor': is_editor}
+
+    def get_saas_auth(self):
+        saas_authorize = config.get(
+            'ckan.saas_portal_quote.saas_authorize', '')
+        return {'saasportal_authorize': saas_authorize}
